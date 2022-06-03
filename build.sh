856998a9738a63d0b81b770bbb528d9dcad2c7a4
Joe Swantek <joseph.swantek@northwestern.edu>
2022-06-03 16:58:24 -0500

    SAGE-1354: remove "VERSION" & click requirement from main script

--- build.sh
+++ build.sh
@@ -3,6 +3,5 @@
 docker run --rm \
   -e NAME="waggle-bk-registration" \
   -e DESCRIPTION="Register with Beekeeper server" \
-  -e "DEPENDS=python3-click" \
   -v "$PWD:/repo" \
   waggle/waggle-deb-builder:latest
